2ca69791f857d4a0415d3644b750cf06c9dc43a7
Aslam <kv.aslam@gmail.com>
2021-04-22 09:12:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# git-auto-semver
+
+Automate semantic versioning in any git repository.
+
+> Pre-requisites
+
+```bash
+chmod +x ./git-auto-semver
+```
+
+> Usage
+
+```bash
+./git-auto-semver
+```
---
+++ git-auto-semver.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+[ ! -d .git ] && echo "Not inside a git repository!" && exit 1
+
+current_version=$(git describe --abbrev=0 --tags 2>/dev/null | sed 's/[^0-9\.]//g')
+current_version_bits=(${current_version//./ })
+
+major=${current_version_bits[0]}
+minor=${current_version_bits[1]}
+patch=${current_version_bits[2]}
+
+[ -z $major ] && major=0
+[ -z $minor ] && minor=0
+[ -z $patch ] && patch=0
+
+latest_merge_message=$(git log --pretty="%s" master -1)
+mode='minor'
+
+[[ $latest_merge_message =~ 'Fixed' ]] && mode='patch'
+[[ $latest_merge_message =~ 'Release' ]] && mode='major'
+
+case "$mode" in
+    "major") major=$((major+1)) && minor=0 && patch=0 ;;
+    "minor") minor=$((minor+1)) && patch=0 ;;
+    "patch") patch=$((patch+1)) ;;
+esac
+new_version='v'$major'.'$minor'.'$patch
+
+git tag -a $new_version -m "$latest_merge_message"
+echo -e '\E[47;31m'"\033[1mReleasing version $new_version\033[0m"
+git push --tags
