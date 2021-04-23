af1d2b3304678295ada89d97e6e0a0722152aeac
Aslam <kv.aslam@gmail.com>
2021-04-23 07:31:07 +0530

    Fixed releasing already released version

--- git-auto-semver.sh
+++ git-auto-semver.sh
@@ -2,7 +2,21 @@
 
 [ ! -d .git ] && echo "Not inside a git repository!" && exit 1
 
-current_version=$(git describe --abbrev=0 --tags 2>/dev/null | sed 's/[^0-9\.]//g')
+current_version=$(git describe --abbrev=0 --tags 2>/dev/null)
+current_version_commit=$(git rev-list -n 1 $current_version 2>/dev/null)
+while [ -z $current_version_commit ]
+do
+  current_version=$(git describe --abbrev=0 --tags $current_version^ 2>/dev/null)
+  if [ -z $current_version ]; then
+    break
+  fi
+  current_version_commit=$(git rev-list -n 1 $current_version 2>/dev/null)
+done
+
+latest_commit=$(git log --format="%H" -n 1)
+[[ "$latest_commit" = "$current_version_commit" ]] && echo "No changes made to release!" && exit 1
+
+current_version=$(echo $current_version | sed 's/[^0-9\.]//g')
 current_version_bits=(${current_version//./ })
 
 major=${current_version_bits[0]}
@@ -26,6 +40,12 @@ case "$mode" in
 esac
 new_version='v'$major'.'$minor'.'$patch
 
+new_version_commit=$(git rev-list -n 1 $new_version 2>/dev/null)
+
+[[ "$current_version_commit" = "$new_version_commit" ]] && echo "v$current_version already released!" && exit 1
+
+git tag -d $(git tag -l)
+git fetch
 git tag -a $new_version -m "$latest_commit_message"
 echo -e '\E[47;31m'"\033[1mReleasing version $new_version\033[0m"
 git push --tags
