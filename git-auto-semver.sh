3122388821dca6907035378f899a092ab6600287
Aslam <kv.aslam@gmail.com>
2021-05-01 13:30:42 +0530

    Automate semantic versioning based on git log history

--- git-auto-semver.sh
+++ git-auto-semver.sh
@@ -9,50 +9,27 @@ cd $path
 
 [ ! -d .git ] && echo "Not inside a git repository!" && exit 1
 
-current_version=$(git describe --abbrev=0 --tags 2>/dev/null)
-current_version_commit=$(git rev-list -n 1 $current_version 2>/dev/null)
-while [ -z $current_version_commit ]
+major=0
+minor=0
+patch=0
+
+git pull --tags
+
+IFS=$'\n'
+for commit_message in $(git log --pretty="%s" --no-merges --reverse);
 do
-  current_version=$(git describe --abbrev=0 --tags $current_version^ 2>/dev/null)
-  if [ -z $current_version ]; then
-    break
-  fi
-  current_version_commit=$(git rev-list -n 1 $current_version 2>/dev/null)
+  mode='minor'
+  [[ $commit_message =~ 'Fixed' ]] && mode='patch'
+  [[ $commit_message =~ 'Released' ]] && mode='major'
+
+  case "$mode" in
+      "major") major=$((major+1)) && minor=0 && patch=0 ;;
+      "minor") minor=$((minor+1)) && patch=0 ;;
+      "patch") patch=$((patch+1)) ;;
+  esac
+  version='v'$major'.'$minor'.'$patch
+  version_commit=$(git rev-list -n 1 $version 2>/dev/null)
+  [[ ! -z $version_commit ]] && git tag -a $version -m "$commit_message"
 done
 
-latest_commit=$(git log --format="%H" -n 1)
-[[ "$latest_commit" = "$current_version_commit" ]] && echo "No changes made to release!" && exit 1
-
-current_version=$(echo $current_version | sed 's/[^0-9\.]//g')
-current_version_bits=(${current_version//./ })
-
-major=${current_version_bits[0]}
-minor=${current_version_bits[1]}
-patch=${current_version_bits[2]}
-
-[ -z $major ] && major=0
-[ -z $minor ] && minor=0
-[ -z $patch ] && patch=0
-
-latest_commit_message=$(git log --pretty="%s" -1 --no-merges)
-mode='minor'
-
-[[ $latest_commit_message =~ 'Fixed' ]] && mode='patch'
-[[ $latest_commit_message =~ 'Release' ]] && mode='major'
-
-case "$mode" in
-    "major") major=$((major+1)) && minor=0 && patch=0 ;;
-    "minor") minor=$((minor+1)) && patch=0 ;;
-    "patch") patch=$((patch+1)) ;;
-esac
-new_version='v'$major'.'$minor'.'$patch
-
-new_version_commit=$(git rev-list -n 1 $new_version 2>/dev/null)
-
-[[ "$current_version_commit" = "$new_version_commit" ]] && echo "v$current_version already released!" && exit 1
-
-git tag -d $(git tag -l)
-git pull --tags
-git tag -a $new_version -m "$latest_commit_message"
-echo -e '\E[47;31m'"\033[1mReleasing version $new_version\033[0m"
 git push --tags
